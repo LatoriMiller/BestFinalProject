bf1900e127b8bf6ac3a7932790712ebc3713c76e
LatoriMiller <latorimiller@Latoris-MacBook-Pro-2.local>
2017-01-06 10:11:41 -0500

    adds homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'all' => 'storefront#all_items'
+
+  get 'categorical' => 'storefront#items_by_category'
+
+  get 'price' => 'storefront#items_by_price'
+
+  # not sure if the route below is correct or necessary
   get 'categories/index'
 
   resources :ads
   devise_for :users
   resources :users
   resources :categories
-  root "ads#index"
+  
+  root "storefront#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
