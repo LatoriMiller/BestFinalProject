b2ba7572fae356c30cf228f9356b84c3788fb232
LatoriMiller <latorimiller@Latoris-MacBook-Pro-2.local>
2017-01-08 21:27:27 -0500

    adds categorical filtering functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :authenticate_user!
+  before_action :categories
+
+  before_action :authenticate_user!
+
+  def categories
+    @categories = Category.all
+  end
+
 end
--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -4,9 +4,44 @@ class StorefrontController < ApplicationController
     @ads = Ad.all
   end
 
+  # def items_by_category
+  #   @category = Category.find(params[:cat_id])
+  #   @ads = Ad.all
+  #   @ads_by_cat = []
+  #   @ads.each do |ad|
+  #     if ad.category.id == params[:cat_id].to_i
+  #       @ads_by_cat.push(ad)
+  #     end
+  #   end
+  # end
+  # print "Enter your grade: "
+  # grade = gets.chomp
+  # case grade
+  # when "A"
+  #   puts 'Well done!'
+  # when "B"
+  #   puts 'Try harder!'
+  # when "C"
+  #   puts 'You need help!!!'
+  # else
+  #   puts "You just making it up!"
+  # end
+
+
+
+# Change to an case statement that checks(against numbers 1-8) an instance of an ad and its category_id from the Ad table (ad.category_id)
   def items_by_category
+    @category = Category.find(params[:cat_id].to_i)
+    @ads = Ad.all
+    @ads_by_cat = []
+      @ads.each do |ad|
+         if ad.category_id == params[:cat_id].to_i
+          @ads_by_cat.push(ad)
+        end
+
+    end
   end
 
-  def items_by_price
-  end
+
+
 end
