c02f0a0395ae3660fd875f3160f241961ca87f37
LatoriMiller <latorimiller@Latoris-MacBook-Pro-2.local>
2017-01-10 09:00:19 -0500

    adds devise views, allows user to edit and destroy their account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,17 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   def categories
     @categories = Category.all
   end
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:signup, keys: [:name, :phone])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name, :phone])
+  end
 end
